e9c727e4006d5fab88075eddce085e20d868f09f
Tigre Mitre <horaciomitre7@gmail.com>
2021-07-21 15:40:08 -0300

    y ella

--- src/components/Productos/ListaProductos.js
+++ src/components/Productos/ListaProductos.js
@@ -1,9 +1,21 @@
 import React, { Fragment } from "react";
+import { Container, ListGroup } from "react-bootstrap";
 
 const ListaProductos = () => {
-  return <Fragment>
-      
-  </Fragment>;
+  return (
+    <Fragment>
+      <Container className='my-5'>
+          <h1 className='text-center mb-5'>Lista productos</h1>
+        <ListGroup>
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+      </Container>
+    </Fragment>
+  );
 };
 
 export default ListaProductos;
