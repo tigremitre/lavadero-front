881b6d0aabfa8ce0d19907f8638716d0068753be
Tigre Mitre <horaciomitre7@gmail.com>
2021-07-22 16:33:55 -0300

    se agregaron cosas

--- src/components/Productos/ItemProducto.js
+++ src/components/Productos/ItemProducto.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { ListGroup, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {}
 
 const ItemProducto = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <ListGroup.Item className='d-flex justify-content-between'>
+      <p>Lavado Express <b>$700</b></p> 
+  <div>
+     <Button variant='warning mr-3'>Editar</Button>
+     <Button variant='danger'>Borrar</Button>
+  </div>
+  </ListGroup.Item>;
 };
 
 export default ItemProducto;
--- src/components/Productos/ListaProductos.js
+++ src/components/Productos/ListaProductos.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Container, ListGroup } from "react-bootstrap";
+import ItemProducto from './ItemProducto';
 
 const ListaProductos = () => {
   return (
@@ -7,11 +8,7 @@ const ListaProductos = () => {
       <Container className='my-5'>
           <h1 className='text-center mb-5'>Lista productos</h1>
         <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          <ItemProducto></ItemProducto>
         </ListGroup>
       </Container>
     </Fragment>
