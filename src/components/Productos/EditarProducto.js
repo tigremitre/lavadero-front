147081a50311dea8c935142170de4c70bcadf3e5
Tigre Mitre <horaciomitre7@gmail.com>
2021-08-05 18:16:55 -0300

    se realizo la consulta del dervicio a editar y se muestran los valores del servicio a editar anasheeee

--- src/components/Productos/EditarProducto.js
+++ src/components/Productos/EditarProducto.js
@@ -1,7 +1,34 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import { Container, Form, Button, Alert } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const EditarProducto = () => {
+  //obtengo el parametro de la url
+
+  const { id } = useParams();
+  const URL = process.env.REACT_APP_API_URL;
+
+  //declaro los state
+  const [producto, setProducto] = useState({});
+
+  //traer los datos del objeto editar
+  useEffect(() => {
+    consultarProducto();
+  }, []);
+
+  const consultarProducto = async () => {
+    try {
+      const respuesta = await fetch(URL + "/" + id);
+      console.log(respuesta);
+      if (respuesta.status === 200) {
+        const resultado = await respuesta.json();
+        setProducto(resultado);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const [categoria, setCategoria] = useState("");
 
   const cambiarCategoria = (e) => {
@@ -12,14 +39,14 @@ const EditarProducto = () => {
     <Fragment>
       <Container className="my-4">
         <Form>
-          <h1 className="my-4 text-center">Agregar Servico</h1>
+          <h1 className="my-4 text-center">Editar Servico</h1>
           <Form.Group>
             <Form.Label>Nombre del Servicio*</Form.Label>
-            <Form.Control type="text" placeholder="Lavado"></Form.Control>
+            <Form.Control type="text" placeholder="Lavado" defaultValue={producto.nombreProducto}></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>Precio*</Form.Label>
-            <Form.Control type="number" placeholder="$1800"></Form.Control>
+            <Form.Control type="number" placeholder="$1800" defaultValue={producto.precioProducto}></Form.Control>
           </Form.Group>
           <div className="text-center my-4">
             <h3>Categoria*</h3>
@@ -30,6 +57,7 @@ const EditarProducto = () => {
               name="categoria"
               value="premium"
               onChange={cambiarCategoria}
+              defaultChecked={producto.categoria && producto.categoria === 'premium'}
             ></Form.Check>
             <Form.Check
               inline
@@ -38,6 +66,7 @@ const EditarProducto = () => {
               name="categoria"
               value="completo"
               onChange={cambiarCategoria}
+              defaultChecked={producto.categoria && producto.categoria === 'completo'}
             ></Form.Check>
             <Form.Check
               inline
@@ -46,6 +75,7 @@ const EditarProducto = () => {
               name="categoria"
               value="chasisCarroceria"
               onChange={cambiarCategoria}
+              defaultChecked={producto.categoria && producto.categoria === 'chasisCarroceria'}
             ></Form.Check>
             <Form.Check
               inline
@@ -54,10 +84,11 @@ const EditarProducto = () => {
               name="categoria"
               value="carroceria"
               onChange={cambiarCategoria}
+              defaultChecked={producto.categoria && producto.categoria === 'carroceria'}
             ></Form.Check>
           </div>
           <Button variant="danger w-100" type="submit">
-            Agregar
+            Guardar
           </Button>
         </Form>
       </Container>
